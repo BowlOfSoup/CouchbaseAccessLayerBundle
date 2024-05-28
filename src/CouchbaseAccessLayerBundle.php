cd8b110ea1152a0fb1ac997493df5820034d06ca
Stefan Koopmanschap <stefan.koopmanschap@tenzinger.com>
2024-05-28 14:19:14 +0200

    Register the extension manually

--- src/CouchbaseAccessLayerBundle.php
+++ src/CouchbaseAccessLayerBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace BowlOfSoup\CouchbaseAccessLayerBundle;
 
+use BowlOfSoup\CouchbaseAccessLayerBundle\DependencyInjection\CouchbaseAccessLayerExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CouchbaseAccessLayerBundle extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new CouchbaseAccessLayerExtension();
+    }
 }
