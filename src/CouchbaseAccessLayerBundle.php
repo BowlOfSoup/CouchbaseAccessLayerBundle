87c24d8c4aa09476312663b9e32737e0fdd3ee46
Stefan Koopmanschap <stefan@ingewikkeld.net>
2024-05-28 14:29:22 +0200

    Fix the Configuration class where an upgrade step was forgotten (#9)
    Fix the Configuration class where an upgrade step was forgotten

--- src/CouchbaseAccessLayerBundle.php
+++ src/CouchbaseAccessLayerBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace BowlOfSoup\CouchbaseAccessLayerBundle;
 
+use BowlOfSoup\CouchbaseAccessLayerBundle\DependencyInjection\CouchbaseAccessLayerExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class CouchbaseAccessLayerBundle extends AbstractBundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new CouchbaseAccessLayerExtension();
+    }
 }
