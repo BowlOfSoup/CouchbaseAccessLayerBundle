f5213a31aea750d72fcad46eb903f9cfee1af84f
Stefan Koopmanschap <stefan@ingewikkeld.net>
2024-05-28 12:51:40 +0200

    Upgrade to symfony 7 (And PHP 8.3) (#8)

--- src/CouchbaseAccessLayerBundle.php
+++ src/CouchbaseAccessLayerBundle.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace BowlOfSoup\CouchbaseAccessLayerBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class CouchbaseAccessLayerBundle extends Bundle
+class CouchbaseAccessLayerBundle extends AbstractBundle
 {
 }
